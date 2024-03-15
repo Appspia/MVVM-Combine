f4ac12c4c1187cb4c775ce53341d6938c9596e66
Appspia <appspia@gmail.com>
2024-03-16 08:15:20 +0900

    feat : multipart 기능 추가

--- TopApp/Core/URLRequest+RequestItem.swift
+++ TopApp/Core/URLRequest+RequestItem.swift
@@ -18,6 +18,7 @@ class URLRequestItem {
     enum ContentType: String {
         case wwwFormUrlencoded = "application/x-www-form-urlencoded"
         case json = "application/json"
+        case mulipartFormData = "multipart/form-data; boundary=BOUNDARY"
     }
     
     let urlString: String
@@ -25,6 +26,42 @@ class URLRequestItem {
     var headerFields: [String: String] = [:]
     var parameters: [String: Any] = [:]
     var contetType: ContentType = .wwwFormUrlencoded
+    
+    // MARK: - Form Data
+    struct FormDataItem {
+        enum FormDataType {
+            case data
+            case png
+            case jpg
+            case custom(String)
+            
+            var contentType: String {
+                switch self {
+                case .data:
+                    return "application/octet-stream"
+                case .png:
+                    return "image/png"
+                case .jpg:
+                    return "image/jpeg"
+                case let .custom(value):
+                    return value
+                }
+            }
+        }
+
+        var type: FormDataType
+        var name: String
+        var data: Data
+        var fileName: String
+        
+        init(type: FormDataType, name: String, data: Data, fileName: String?) {
+            self.type = type
+            self.name = name
+            self.data = data
+            self.fileName = fileName ?? NSUUID().uuidString
+        }
+    }
+    var formDataItems: [FormDataItem] = []
     
     // MARK: - Options
     var cachePolicy: URLRequest.CachePolicy = .reloadIgnoringLocalCacheData
@@ -57,6 +94,23 @@ extension URLRequest {
                 if let jsonData = try? JSONSerialization.data(withJSONObject: item.parameters, options: []) {
                     body = jsonData
                 }
+            } else if item.contetType == .mulipartFormData {
+                var formBody = Data()
+                for (key, value) in item.parameters {
+                    formBody.append(string: "--BOUNDARY\r\n")
+                    formBody.append(string: "Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                    formBody.append(string: "\(value)\r\n")
+                }
+                
+                for item in item.formDataItems {
+                    formBody.append(string: "--BOUNDARY\r\n")
+                    formBody.append(string: "Content-Disposition: form-data; name=\"\(item.name)\"; filename=\"\(item.fileName)\"\r\n")
+                    formBody.append(string: "Content-Type: \(item.type.contentType)\r\n\r\n")
+                    formBody.append(item.data)
+                    formBody.append(string: "\r\n")
+                }
+                formBody.append(string: "--BOUNDARY--\r\n")
+                body = formBody
             }
         }
 
@@ -65,5 +119,13 @@ extension URLRequest {
         httpMethod = item.httpMethod.rawValue
         allHTTPHeaderFields = item.headerFields
         httpBody = body
+    }
+}
+
+extension Data {
+    mutating func append(string: String) {
+        if let data = string.data(using: .utf8) {
+            self.append(data)
+        }
     }
 }
